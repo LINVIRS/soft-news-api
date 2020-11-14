42e6ddb2777aa461e48f7f374bcbf82935d1159d
Administrator <admin@example.com>
2020-11-14 18:48:15 +0800

    日志模块 以及多环境启动

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.HelloControllerApi;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version: V1.0
  **/
 @RestController
+@Slf4j
 public class HelloController implements HelloControllerApi {
 
     @Override
     public Object hello() {
+        log.info("info ,hello");
+        log.warn("warn hello");
+        log.error("error hello");
         return "hello 你好啊";
     }
 }
