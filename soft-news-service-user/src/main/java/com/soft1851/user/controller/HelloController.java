45f575b69c6820c5768590d92de6083e388f708e
Administrator <admin@example.com>
2020-11-15 21:42:01 +0800

    redis测试

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -2,8 +2,12 @@ package com.soft1851.user.controller;
 
 import com.soft1851.api.user.HelloControllerApi;
 import com.soft1851.result.GraceResult;
+import com.soft1851.utils.RedisOperator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @ClassName: HelloController
@@ -15,12 +19,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class HelloController implements HelloControllerApi {
-
+@Resource
+private RedisOperator redis;
     @Override
     public Object hello() {
         log.info("info ,hello");
         log.warn("warn hello");
         log.error("error hello");
         return GraceResult.ok("hello");
+    }
+
+    /**
+     * redis测试
+     * @return
+     */
+    @GetMapping("redis")
+    public GraceResult redis(){
+        redis.set("age","20");
+        return  GraceResult.ok(redis.get("age"));
     }
 }
